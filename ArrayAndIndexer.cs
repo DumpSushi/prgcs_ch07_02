16abf75f64c199915c737df4624bb6a3cb12b6e9
hide_m <hide_m@ym.tose.co.jp>
2015-10-27 17:14:55 +0900

    P245:例7-27　コンパイラの解釈と同等なコード.

---
+++ ArrayAndIndexer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch07_02
+{
+	class ArrayAndIndexer<T>
+	{
+		public T[] TheArray = new T[100];
+
+		public T this[int index]
+		{
+			get
+			{
+				return TheArray[index];
+			}
+			set
+			{
+				TheArray[index] = value;
+			}
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -10,11 +10,44 @@ namespace ch07_02
 	{
 		static void Main(string[] args)
 		{
+			//
 			Indexable ix = new Indexable();
 			Console.WriteLine(ix[10]);
 			ix[42] = "Xyzzy";
 
+			//
+			ImmutableValue();
+
+			//
+			MutableValue();
+
 			Console.ReadKey();
+		}
+
+		static void ImmutableValue()
+		{
+			ArrayAndIndexer<int> aai = new ArrayAndIndexer<int>();
+			aai.TheArray[10] = 42;
+			Console.WriteLine(aai[10]);
+			aai[20] = 99;
+			Console.WriteLine(aai.TheArray[20]);
+		}
+
+		static void MutableValue()
+		{
+			ArrayAndIndexer<CanChange> aai = new ArrayAndIndexer<CanChange>();
+			aai.TheArray[10] = new CanChange { Number = 42 };
+			Console.WriteLine(aai[10].Number);
+			aai[20] = new CanChange { Number = 99, Name = "独自の要素"};
+			Console.WriteLine(aai.TheArray[20].Number);
+
+			aai.TheArray[10].Number = 123;
+			Console.WriteLine(aai.TheArray[10].Number);
+
+			//aai[20].Number = 456;
+			CanChange elem = aai[20];
+			elem.Number = 456;
+			Console.WriteLine(aai.TheArray[20].Number);
 		}
 	}
 }
